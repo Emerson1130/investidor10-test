08677de8c6ecf023b9fff4d046cdd9190ec5c555
zevitagem <jose.vsilva@mercadolivre.com>
2023-02-21 10:43:00 -0300

    Ajusted migration

--- database/migrations/2023_01_14_182743_create_posts_table.php
+++ database/migrations/2023_01_14_182743_create_posts_table.php
@@ -13,11 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('books', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->integer('isbn');
-            $table->decimal('value');
+            $table->string('title');
+            $table->text('body');
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
@@ -32,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('books');
+        Schema::dropIfExists('posts');
     }
 };
